df4358e4ac77f1cdd13efb7f53203cce46c95d54
Zach Nicoll <zaxternoob@gmail.com>
2019-10-29 09:12:01 +1000

    Add 2 Projects

---
+++ nicollzcom/js/mouseOverAnims.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var bkg;
+    $(".portfolio-box").hover(
+        function () {
+            bkg = $(this).css('background-image');
+            $(this).css("background-image","none");
+            $(this).find('h2').css("opacity","0");
+            $(this).find('p').css("opacity","1");
+            $(this).find('i').css("opacity","1");
+        },
+        function () {
+            $(this).css("background-image", bkg);
+            $(this).find('h2').css("opacity","1");
+            $(this).find('p').css("opacity","0");
+            $(this).find('i').css("opacity","0");
+        }
+    );
+});
