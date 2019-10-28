c3e38fcbe5559487e2f8d54053f54cdb37f9d4e6
Zach Nicoll <zaxternoob@gmail.com>
2019-10-28 22:18:36 +1000

    Fading in portfolio slides.

--- nicollzcom/js/fadeInOnScroll.js
+++ nicollzcom/js/fadeInOnScroll.js
@@ -1,15 +1,28 @@
-$(document).ready(function(){
-    $(function(){
-        $('.title').animate({'opacity':'1'}, 1500);
+$(document).ready(function () {
+    var offset = 0;
+
+    $(function () {
+        $('.title').animate({
+            'opacity': '1'
+        }, 1500);
+        $('.portfolio-box').each(function(i){
+            offset += 500;
+            $(this).animate({
+                'opacity': '1',
+                'bottom':'0%'
+            }, 1500 + offset);
+        });
     });
-    $(window).scroll( function(){
-        $('.container').each( function(i){
+    $(window).scroll(function () {
+        $('.container').each(function (i) {
             var bottom_object = $(this).position().top + $(this).outerHeight()/2;
             var bottom_window = $(window).scrollTop() + $(window).height();
-
-            if(bottom_window > bottom_object){
-                $(this).animate({'opacity':'1'},1500);
-            }
+            console.log(bottom_window, bottom_object);
+            if ($(this), bottom_window > bottom_object) {
+                $(this).animate({
+                    'opacity': '1'
+                }, 1500);
+            } 
         });
     });
 });
