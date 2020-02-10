f042353fc7d71c3aab8762153a0e46fcf63e984b
Zach Nicoll <zaxternoob@gmail.com>
2020-02-10 11:26:22 +1000

    Added 10-word filter to messages to stop spam.

--- nicollzcom/contact-form-handler.php
+++ nicollzcom/contact-form-handler.php
@@ -3,15 +3,18 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'nicollzc@srv.coastserver.com.au';
-    $email_subject = "Form Submission";
-    $email_body = "Name: $name\n".
-                    "Email: $email\n".
-                    "Message: \n$message\n";
-    
-    $to = "zach.nicoll55@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $email \r\n";
-    mail($to, $email_subject, $email_body, $headers);
+    $words = explode(' ', $message);
+    if(count($words) > 10){
+        $email_from = 'nicollzc@srv.coastserver.com.au';
+        $email_subject = "Form Submission";
+        $email_body = "Name: $name\n".
+                        "Email: $email\n".
+                        "Message: \n$message\n";
+        
+        $to = "zach.nicoll55@gmail.com";
+        $headers = "From: $email_from \r\n";
+        $headers .= "Reply-To: $email \r\n";
+        mail($to, $email_subject, $email_body, $headers);
+    }
     header("Location: ./index.html");
 ?>
